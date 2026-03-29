82ead56c145d8ff08a343fbd05fc33c0419bbed8
Max J. Polster <max.polster@id-p.de>
2026-03-29 14:28:27 +0200

    Flex space

--- src/components/flex-space.tsx
+++ src/components/flex-space.tsx
@@ -1,14 +1,13 @@
+import styles from "@rvx/ui/theme/components/flex-space.module.css";
 import { Expression, get } from "rvx";
-import { THEME } from "../common/theme.js";
 
 export function FlexSpace(props: {
 	grow?: Expression<number | undefined>;
 }): unknown {
-	const theme = THEME.current;
 	return <div
-		class={theme?.flex_space}
+		class={styles.flex_space}
 		style={{
-			"flex-grow": () => String(get(props.grow) ?? 1),
+			"flex-grow": () => get(props.grow) ?? 1,
 		}}
 	/>;
 }
